977cd05333a7f5055db74852bd04075c01de50d7
gging87z <gging87z@gmail.com>
2020-03-22 18:57:37 +0800

    Init

---
+++ Personal Director/View/HomePage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Personal_Director.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Personal_Director
+{
+    public sealed partial class HomePage : Page
+    {
+        private Project project { get; set; }
+
+        public HomePage()
+        {
+            this.InitializeComponent();
+            this.project = new Project()
+            {
+                Name = "123"
+            };
+        }
+
+        private void PrePage_Click(object sender, RoutedEventArgs e)
+        {
+            On_BackRequested();
+        }
+
+        private bool On_BackRequested()
+        {
+            this.project.Name = this.project.Name + "1";
+
+            if (this.Frame.CanGoBack)
+            {
+                this.Frame.GoBack();
+                return true;
+            }
+            return false;
+        }
+
+        private void NewProject_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ProjectEdit), project);
+        }
+    }
+}
---
+++ Personal Director/View/ProjectEdit.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Personal_Director.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Personal_Director
+{
+    public sealed partial class ProjectEdit : Page
+    {
+        private Project project { get; set; }
+
+        public ProjectEdit()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PrePage_Click(object sender, RoutedEventArgs e)
+        {
+            On_BackRequested();
+        }
+
+        private bool On_BackRequested()
+        {
+            this.project.Name = this.project.Name + "1";
+
+            if (this.Frame.CanGoBack)
+            {
+                this.Frame.GoBack();
+                return true;
+            }
+            return false;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter is Project)
+            {
+                project = (Project)e.Parameter;
+            }
+            else
+            {
+                throw new Exception("ne project !!");
+            }
+            base.OnNavigatedTo(e);
+        }
+    }
+}
